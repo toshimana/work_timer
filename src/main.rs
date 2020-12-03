ab926fee7b29f43163166e2c03fa6801fea9954a
toshimana <toshi.mana.plus1@gmail.com>
2020-12-03 22:59:17 +0900

    init project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use iced::{executor, Application, Command, Element, Text};
+use iced::Settings;
+
+struct GUI;
+
+impl Application for GUI {
+    type Executor = executor::Null;
+    type Message = ();
+    type Flags = ();
+
+    fn new(_flaggs: ()) -> (GUI, Command<Self::Message>) {
+        (GUI, Command::none())
+    }
+
+    fn title(&self) -> String {
+        String::from("DEMO")
+    }
+
+    fn update(&mut self, _message: Self::Message) -> Command<Self::Message> {
+        Command::none()
+    }
+
+    fn view(&mut self) -> Element<Self::Message> {
+        Text::new("Hello, World!").into()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    GUI::run(Settings::default());
 }
