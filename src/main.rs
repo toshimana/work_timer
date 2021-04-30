087dd46baebb596a9a5e1084ca8eb7c8ac45459b
toshimana <toshi.mana.plus1@gmail.com>
2021-04-30 22:40:48 +0900

    AudioPlayerの実装

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::fs::File;
+use std::sync::mpsc;
+use std::io::BufReader;
 use std::time::Duration;
 use std::time::Instant;
 use iced::{button, executor, futures, Align, Application, Button, Column, Command, Clipboard, Element, Font, HorizontalAlignment, Length, Row, Settings, Subscription, Text};
+use rodio::{Decoder, OutputStream, source::Source, Sink};
 
 const FPS: u64 = 30;
 const MILLISEC: u64 = 1000;
@@ -11,6 +15,136 @@ const FONT: Font = Font::External{
     name: "PixelMplus12-Regular",
     bytes: include_bytes!("../rsc/PixelMplus12-Regular.ttf"),
 };
+
+
+enum MusicPlayerMessage {
+    Quit,
+    Initialize(String),
+    Play,
+    Stop,
+}
+
+enum MusicPlayerState {
+    UninitializeState,
+    StopState,
+    PlayState,
+}
+
+struct MusicPlayer {
+    state: MusicPlayerState,
+    stream: Option<rodio::OutputStream>,
+    sink: Option<rodio::Sink>,
+}
+
+impl MusicPlayer {
+    pub fn new() -> MusicPlayer {
+        MusicPlayer {
+            state: MusicPlayerState::UninitializeState,
+            stream: None,
+            sink: None,
+        }
+    }
+    pub fn run(&mut self, rx : mpsc::Receiver<MusicPlayerMessage>) {
+        let mut loop_flag = true;
+        while loop_flag {
+            let ret = rx.recv_timeout(Duration::from_millis(10));
+            match ret {
+                Ok(msg) => {
+                    match msg {
+                        MusicPlayerMessage::Quit => {
+                            loop_flag = false
+                        }
+                        MusicPlayerMessage::Initialize(path) => {
+                            self.initialize(path)
+                        }
+                        MusicPlayerMessage::Play => {
+                            self.play()
+                        }
+                        MusicPlayerMessage::Stop => {
+                            self.stop()
+                        }
+                    }
+                }
+                Err(_) => {
+                }
+            }
+        }
+    }
+    pub fn initialize(&mut self, path: String) {
+        {
+            let (stream, stream_handle) = OutputStream::try_default().unwrap();
+            self.stream = Option::from(stream);
+            let sink = Sink::try_new(&stream_handle).unwrap();
+            let file = BufReader::new(File::open(path).unwrap());
+            let source = Decoder::new(file).unwrap().repeat_infinite();
+            sink.append(source);
+
+            self.state = MusicPlayerState::StopState;
+            self.sink = Option::from(sink);
+        }
+    }
+    pub fn play(&mut self) {
+        match self.state {
+            MusicPlayerState::StopState => {
+                if let Some(s) = &self.sink {
+                    s.play();
+                }
+                self.state = MusicPlayerState::PlayState
+            }
+            _ => {}
+        }
+    }
+    pub fn stop(&mut self) {
+        match self.state {
+            MusicPlayerState::PlayState => {
+                if let Some(s) = &self.sink {
+                    s.pause();
+                }
+                self.state = MusicPlayerState::StopState
+            }
+            _ => {}
+        }
+    }
+}
+
+struct MusicPlayerActor {
+    thread: Option<std::thread::JoinHandle<()>>,
+    tx: Option<mpsc::Sender<MusicPlayerMessage>>,
+}
+
+impl MusicPlayerActor {
+    pub fn new() -> MusicPlayerActor {
+        MusicPlayerActor {
+            thread : None,
+            tx : None,
+        }
+    }
+
+    pub fn start(&mut self) {
+        let (tx, rx) = mpsc::channel();
+        self.tx = Option::from(tx);
+        self.thread = Option::from(std::thread::spawn(move || MusicPlayer::new().run(rx)));
+    }
+
+    fn send_message(&mut self, msg: MusicPlayerMessage) {
+        if let Some(tx) = &self.tx {
+            let _ = tx.send(msg);
+        }
+    }
+
+    pub fn quit(&mut self) {
+        self.send_message(MusicPlayerMessage::Quit);
+    }
+    pub fn initialize(&mut self, path: String) {
+        self.send_message(MusicPlayerMessage::Initialize(path));
+    }
+    pub fn play(&mut self) {
+        self.send_message(MusicPlayerMessage::Play);
+    }
+    pub fn stop(&mut self) {
+        self.send_message(MusicPlayerMessage::Stop);
+    }
+}
 
 pub struct Timer {
     duration: Duration,
@@ -225,7 +359,24 @@ impl Application for GUI {
     }
 }
 
+
 fn main() {
+    let mut player = MusicPlayerActor::new();
+    player.start();
+    player.initialize("alert.mp3".to_string());
+    player.play();
+    std::thread::sleep(Duration::from_secs(1));
+    player.stop();
+    std::thread::sleep(Duration::from_secs(1));
+    player.play();
+    std::thread::sleep(Duration::from_secs(1));
+    player.stop();
+    std::thread::sleep(Duration::from_secs(1));
+    player.play();
+    std::thread::sleep(Duration::from_secs(1));
+    player.stop();
+    std::thread::sleep(Duration::from_secs(1));
+
     let mut settings = Settings::default();
     settings.window.size = (400u32, 120u32);
     let _result = GUI::run(settings);
