3ee20e01c5ffe5656cfd272e89d5f1ef6b8c212e
toshimana <toshi.mana.plus1@gmail.com>
2021-04-30 23:46:31 +0900

    ファイル分割

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 #![windows_subsystem = "windows"]
 
-use std::fs::File;
-use std::sync::mpsc;
-use std::io::BufReader;
 use std::time::Duration;
 use std::time::Instant;
 use iced::{button, executor, futures, Align, Application, Button, Column, Command, Clipboard, Element, Font, HorizontalAlignment, Length, Row, Settings, Subscription, Text};
-use rodio::{Decoder, OutputStream, source::Source, Sink};
+mod music_player;
+use music_player::MusicPlayerActor;
 
 const FPS: u64 = 30;
 const MILLISEC: u64 = 1000;
@@ -17,135 +15,6 @@ const FONT: Font = Font::External{
     name: "PixelMplus12-Regular",
     bytes: include_bytes!("../rsc/PixelMplus12-Regular.ttf"),
 };
-
-enum MusicPlayerMessage {
-    Initialize(String),
-    Play,
-    Pause,
-}
-
-enum MusicPlayerState {
-    UninitializeState,
-    StopState,
-    PlayState,
-}
-
-struct MusicPlayer {
-    state: MusicPlayerState,
-    stream: Option<rodio::OutputStream>,
-    sink: Option<rodio::Sink>,
-}
-
-impl MusicPlayer {
-    pub fn new() -> MusicPlayer {
-        MusicPlayer {
-            state: MusicPlayerState::UninitializeState,
-            stream: None,
-            sink: None,
-        }
-    }
-    pub fn run(&mut self, rx : mpsc::Receiver<MusicPlayerMessage>) {
-        loop {
-            let ret = rx.recv_timeout(Duration::from_millis(10));
-            match ret {
-                Ok(msg) => {
-                    match msg {
-                        MusicPlayerMessage::Initialize(path) => {
-                            self.initialize(path)
-                        }
-                        MusicPlayerMessage::Play => {
-                            self.play()
-                        }
-                        MusicPlayerMessage::Pause => {
-                            self.pause()
-                        }
-                    }
-                }
-                Err(_) => {
-                }
-            }
-        }
-    }
-    pub fn initialize(&mut self, path: String) {
-        {
-            let (stream, stream_handle) = OutputStream::try_default().unwrap();
-            self.stream = Option::from(stream);
-            let sink = Sink::try_new(&stream_handle).unwrap();
-            let file = BufReader::new(File::open(path).unwrap());
-            let source = Decoder::new(file).unwrap().repeat_infinite();
-            sink.append(source);
-            sink.pause();
-
-            self.state = MusicPlayerState::StopState;
-            self.sink = Option::from(sink);
-        }
-    }
-    pub fn play(&mut self) {
-        match self.state {
-            MusicPlayerState::StopState => {
-                if let Some(s) = &self.sink {
-                    s.play();
-                }
-                self.state = MusicPlayerState::PlayState
-            }
-            _ => {}
-        }
-    }
-    pub fn pause(&mut self) {
-        match self.state {
-            MusicPlayerState::PlayState => {
-                if let Some(s) = &self.sink {
-                    s.pause();
-                }
-                self.state = MusicPlayerState::StopState
-            }
-            _ => {}
-        }
-    }
-}
-
-struct MusicPlayerActor {
-    thread: Option<std::thread::JoinHandle<()>>,
-    tx: Option<mpsc::Sender<MusicPlayerMessage>>,
-}
-
-impl MusicPlayerActor {
-    pub fn new() -> MusicPlayerActor {
-        MusicPlayerActor {
-            thread : None,
-            tx : None,
-        }
-    }
-
-    pub fn new_start_initialize(path:String) -> MusicPlayerActor {
-        let mut obj = MusicPlayerActor::new();
-        obj.start();
-        obj.initialize(path);
-        obj
-    }
-
-    pub fn start(&mut self) {
-        let (tx, rx) = mpsc::channel();
-        self.tx = Option::from(tx);
-        self.thread = Option::from(std::thread::spawn(move || MusicPlayer::new().run(rx)));
-    }
-
-    fn send_message(&mut self, msg: MusicPlayerMessage) {
-        if let Some(tx) = &self.tx {
-            let _ = tx.send(msg);
-        }
-    }
-
-    pub fn initialize(&mut self, path: String) {
-        self.send_message(MusicPlayerMessage::Initialize(path));
-    }
-    pub fn play(&mut self) {
-        self.send_message(MusicPlayerMessage::Play);
-    }
-    pub fn pause(&mut self) {
-        self.send_message(MusicPlayerMessage::Pause);
-    }
-}
 
 pub struct Timer {
     duration: Duration,
